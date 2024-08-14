c3abb83cf293aea15699e34a8aeb3e3faf53a567
aui_tuthousend <mraihan286@gmail.com>
2024-08-14 10:48:20 +0700

    feat: axios

--- src/main.js
+++ src/main.js
@@ -1,8 +1,13 @@
 import './assets/main.css'
-
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
+import axios from 'axios';
+
+window.axios = axios;
+
+window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
+
 
 const app = createApp(App)
 
