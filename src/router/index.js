10bff40c848db81cffd353f1264997c76f2e1439
aui_tuthousend <mraihan286@gmail.com>
2024-08-14 16:20:00 +0700

    feat: import

--- src/router/index.js
+++ src/router/index.js
@@ -1,19 +1,25 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
 import Sheet from "@/components/Sheet.vue";
+import Import from "@/components/import/import.vue";
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
       path: '/',
-      name: 'home',
+      name: 'Home',
       component: HomeView
     },
     {
       path: '/sheet/:jurusan/:periode',
       name: 'Sheet',
       component: Sheet
+    },
+    {
+      path: '/import',
+      name: 'Import',
+      component: Import
     },
   ]
 })
